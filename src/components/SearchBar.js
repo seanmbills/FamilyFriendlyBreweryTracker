b1454d03172815da0b2bb1f81332c8173e903b6f
Sean Bills <seanmichaelbills@gmail.com>
2019-09-30 10:33:26 -0400

    updating the search bar to have a modal that pops up with all possible filter criteria when user clicks the filter button to the right of the search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,79 @@
-import React from 'react'
-import {View, Text, StyleSheet, TextInput} from 'react-native'
+import React, {useState} from 'react'
+import {View, Text, StyleSheet, TextInput, Switch,} from 'react-native'
 import {Feather } from '@expo/vector-icons'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import Modal from 'react-native-modal'
+import { ButtonGroup } from 'react-native-elements'
+import {Rating} from 'react-native-ratings'
+import ModalDropdown from 'react-native-modal-dropdown'
+
+
 
 const SearchBar = ({term, onTermChange, onSearchSubmit}) => {
+    const [modalOpen, setModalOpen] = useState(false)
+    const [openNow, setOpenNow] = useState(false)
+    const [kidFriendlyNow, setKidFriendlyNow] = useState(false)
+    const [maximumPrice, setPrice] = useState(0)
+    const [minimumRating, setRating] = useState(5)
+    const [distance, setDistance] = useState(1)
+    const [useLocation, setUseLocation] = useState(true)
+
+    const priceButtons = ["$", "$$", "$$$", "$$$$"]
+
+    renderModalContent = () => (
+        <View style={styles.content}>
+            <Text style={styles.contentTitle}>Hi 👋!</Text>
+            <View style={{flexDirection: 'row'}}>
+                {/* if the user provides permission to use their location, need to figure out how to pull this information*/}
+                <Text>Use Current Location:</Text>
+                <Switch onValueChange={() => setUseLocation(!useLocation)} value={useLocation}/>
+            </View>
+            
+            {// display an entry field to ask for zipcode entry
+            // and a button that gives the option to use the zipcode associated with the user's account instead
+            // if user enters zipCode, should pull lat/long pair from MapQuest api by sending zipCode with " USA" appended to
+            // end (to filter out potential results from other countries)
+                useLocation ? 
+                <Text style={styles.errorMsg}>{state.errorMessage}</Text>
+                : null
+            }
+            <View style={{flexDirection: 'row'}}>
+                <Text style={styles.filterText}>Open Now</Text>
+                <Switch onValueChange={() => setOpenNow(!openNow)} value={openNow}/>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+                <Text style={styles.filterText}>Kid Friendly Now</Text>
+                <Switch onValueChange={() => setKidFriendlyNow(!kidFriendlyNow)} value={kidFriendlyNow}/>
+            </View>
+            <Text>Maximum Price</Text>
+            <ButtonGroup
+                onPress={(e) => {setPrice(e)}}
+                selectedIndex={maximumPrice}
+                buttons={priceButtons}
+                containerStyle={{height: 100}}
+            />
+            <View>
+                <Text>Minimum Rating</Text>
+                <Rating
+                    imageSize={40}
+                    startingValue={minimumRating}
+                    onFinishRating={(e) => {setRating(e); console.log(e)}}
+                    style={styles.rating}
+                />
+            </View>
+            
+            <View style={{flexDirection: 'row', marginTop: 25}}>
+                <Text>Maximum Distance: </Text>
+                <ModalDropdown 
+                    options={["1 mile", "5 miles", "10 miles", "25 miles", "50 miles"]}
+                    defaultIndex={distance}
+                    defaultValue="5 miles"
+                    onSelect={(index) => {setDistance(parseInt(index))}}
+                />
+            </View>
+        </View>
+    );
+
     return (
         <View style = {styles.searchView}>
             <Feather style={styles.searchIcon} name="search"/>
@@ -15,6 +86,25 @@ const SearchBar = ({term, onTermChange, onSearchSubmit}) => {
                 autoCorrect={false}
                 onEndEditing={onSearchSubmit}
             />
+            <TouchableOpacity onPress={
+                () => setModalOpen(!modalOpen)}
+            >
+                <Feather style={styles.searchIcon} name="filter" />
+            </TouchableOpacity>
+            <Modal
+                isVisible={modalOpen}
+                backdropColor="#B4B3DB"
+                backdropOpacity={0.8}
+                animationIn="zoomInDown"
+                animationOut="zoomOutUp"
+                animationInTiming={600}
+                animationOutTiming={600}
+                backdropTransitionInTiming={600}
+                backdropTransitionOutTiming={600}
+                onBackdropPress={() => setModalOpen(!modalOpen)}
+            >
+                    {this.renderModalContent()}
+            </Modal>
         </View>
     )
 }
@@ -38,7 +128,24 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         marginHorizontal: 10,
 
-    }
+    },
+    filterText: {
+        textAlignVertical:'center',
+        color: 'black',
+
+    },
+    content: {
+        backgroundColor: 'white',
+        padding: 22,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+      },
+      contentTitle: {
+        fontSize: 20,
+        marginBottom: 12,
+      },
 })
 
 export default SearchBar
