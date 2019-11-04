bbe64ff622ccafa211954872ac7720f75e380d47
peteherman <peteherman@yahoo.com>
2019-11-04 15:39:37 -0500

    Added documentaiton to the PasswordField Component

--- src/components/PasswordField.js
+++ src/components/PasswordField.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { TextInput, StyleSheet } from 'react-native';
 
+/* 
+ * password fields need special styling to ensure passwords are not openly displayed
+ * this component essentially is meant to serve as simpler way to implement a password input field
+ * 
+ * @prop password - state object - used to obtain value from this component
+ * @prop onChangeText - state object modifier method - used to modify password @prop
+ */
 const PasswordField = ({password, onChangeText}) => {
     return (
         <TextInput
