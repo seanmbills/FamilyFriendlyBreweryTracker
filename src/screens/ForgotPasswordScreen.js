ba2652179d1f95e21fea6032c3003af9985f1fec
peteherman <peteherman@yahoo.com>
2019-11-04 15:17:11 -0500

    Added documentation the ForgotPasswordScreen

--- src/screens/ForgotPasswordScreen.js
+++ src/screens/ForgotPasswordScreen.js
@@ -1,26 +1,42 @@
+// React native imports
 import React, {useState, useContext} from 'react';
 import {View, StyleSheet, Text, KeyboardAvoidingView} from 'react-native'
-import WelcomeButton from '../components/WelcomeButton';
 import {Context as AuthContext} from '../context/AuthContext';
 import {NavigationEvents} from 'react-navigation';
 import {ScrollView} from 'react-native-gesture-handler';
+import {Input} from 'react-native-elements';
+
+// Local imports
+import WelcomeButton from '../components/WelcomeButton';
 import {validatePassword} from '../api/InputValidation';
 import SubHeading from '../components/SubHeading';
-import {Input} from 'react-native-elements';
+
 
 // NOTE: Had trouble adding KeyboardAvoidingView to this screen but consider trying to add
 // again in the future
 
+/*
+ * Screen will allow user to reset his/her password by first entering an email address, 
+ * and then entering a code (sent to his/her email) in conjunction with a new password. 
+ * The use of this code ensures user authenticity. 
+ */
 const ForgotPasswordScreen = ({navigation}) => {
     const {state, forgotPassword, resetPassword, clearErrorMessage} = useContext(AuthContext)
+
+    // State objects relating to inputs required for reset process
     const [emailOrId, setEmailOrId] = useState('');
     const [resetCode, setResetCode] = useState('');
     const [newPassword, setNewPassword] = useState('');
+    const [confirmPass, setConfirmPass] = useState('');
+
+    // State objects which control what fields are displayed to the user
     const [showEmail, setShowEmail] = useState(true);
     const [showPass, setShowPass] = useState(false);
+
+    // Error messages set by frontend for incorrect/invalid password and email inputs
     const [passErrMsg, setPassErrMsg] = useState('');
     const [emailErrMsg, setEmailErrMsg] = useState('');
-    const [confirmPass, setConfirmPass] = useState('');
+    
         return (
             <ScrollView keyboardDismissMode='on-drag' style={styles.background}>
                 <NavigationEvents
@@ -29,7 +45,10 @@ const ForgotPasswordScreen = ({navigation}) => {
                 <SubHeading 
                     title="Forgot Password"
                 />
-                { showEmail && <View>
+                { /* A user must first enter his or her email before proceeding to the next steps of the reset process.
+                    So first, only an email field is presented to the user
+                  */
+                    showEmail && <View>
                 <View style= {styles.formElement}>
                     <Input
                         value={emailOrId}
@@ -68,7 +87,11 @@ const ForgotPasswordScreen = ({navigation}) => {
                 </View>
                 }
                 {state.errorMessage ? <Text style={styles.errorMsg}>{state.errorMessage}</Text> : null}
-                { showPass && <View>
+                { /* After the user enters an email,  passcode, password, and confirm password fields are displayed to them.
+                   * The frontend does not query the database at all to check if the email entered was a valid email before
+                   * moving on to the next steps. This is done so potential attackers cannot enumerate users in our system
+                   */
+                    showPass && <View>
                     <View>
                         <Text style={styles.resetMsg}>If the email entered exists, a reset code has been sent.
                             Please enter the reset code and a new password.
