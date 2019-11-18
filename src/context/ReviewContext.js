052e65bbccfdd09ed1ae86f18e923052301f0012
peteherman <peteherman@yahoo.com>
2019-11-18 09:28:43 -0500

    Committing new brewery review context methods. Have not tested them for functionality yet

--- src/context/ReviewContext.js
+++ src/context/ReviewContext.js
@@ -2,6 +2,7 @@ import createDataContext from './createDataContext'
 import ServerApi from '../api/Server'
 import {navigate} from '../navigationRef'
 import {AsyncStorage} from 'react-native'
+import Server from '../api/Server'
 
 
 const reviewReducer = (state, action) => {
@@ -16,6 +17,10 @@ const reviewReducer = (state, action) => {
             return {...state, individualResult: null};
         case 'clear_error_message':
             return {...state, errorMessage: ''}
+        case 'review_search':
+            return {...state, count: action.payload.count, single_review: action.payload.response}
+        case 'edit':
+            return {...state, count: action.payload.count, edited: action.payload.response}
         default:
             return state;
     }
@@ -63,11 +68,64 @@ const getBreweryReviews = (dispatch) => {
         }
     }
 }
+
+// const getUserReviews = (dispatch) => {
+//     return async ({userId}) => {
+//         var req = {userId};
+
+//         try {
+//             const response = await ServerApi.get('/')
+//         }
+//     }
+// }
+const getReview = (dispatch) => {
+    return async ({reviewId}) => {
+        var req = {reviewId};
+        try {
+            const response = await ServerApi.get('/review', 
+            {params: req},
+            { headers: {
+              'Accept' : 'application/json', 'Content-type' : 'application/json',
+              'authorization' : 'Bearer ' + (await AsyncStorage.getItem('token'))
+            }});
+            dispatch({type: 'single_review', count: response.data.count, payload: response.data})
+            return response
+        } catch (err) {
+            console.log("Error: ", err.response)
+
+            console.log(err.response.daeta.error);
+            dispatch({type: 'add_error_message', payload: err.response.data.error})
+        }
+    }
+}
+
+const editReview = (dispatch) => {
+    return async({breweryId, message, rating, reviewId}) => {
+        var req = {breweryId, message, rating, reviewId}
+        try {
+            const response = await ServerApi.post('/createReview', 
+            req,
+            { headers: {
+              'Accept' : 'application/json', 'Content-type' : 'application/json',
+              'authorization' : 'Bearer ' + (await AsyncStorage.getItem('token'))
+            }});
+            dispatch({type: 'edit', count: response.data.count, payload: response.data})
+            return response;
+        } catch (err) {
+            console.log("Error: ", err.response)
+
+            console.log(err.response.daeta.error);
+            dispatch({type: 'add_error_message', payload: err.response.data.error})
+        }
+    }
+}
 export const {Provider, Context} = createDataContext(
     reviewReducer,
     {
         createReview,
-        getBreweryReviews
+        getBreweryReviews,
+        getReview,
+        editReview
     },
-    {results: [], count: 0, errorMessage: '', created: '', results: []}
+    {results: [], count: 0, errorMessage: '', created: '', results: [], single_review: null, edited: ''}
 )
