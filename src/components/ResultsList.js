2dd24effc0d10043843e900ac9115b681de35a07
Sean Bills <seanmichaelbills@gmail.com>
2019-11-24 12:21:45 -0500

    added ability to display in-line ad banners in the brewery list search results; will add in interstitial ads later

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -5,26 +5,71 @@ import {withNavigation} from 'react-navigation'
 import {Context as BreweryContext} from '../context/BreweryContext'
 
 
+import {
+    AdMobBanner,
+    AdMobInterstitial,
+    AdMobRewarded,
+    PublisherBanner
+} from 'expo-ads-admob'
+
+async function initializeInterstitial() {
+    AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/4411468910'); // Test ID, Replace with your-admob-unit-id
+    AdMobInterstitial.setTestDeviceID('EMULATOR');
+    try {
+        await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true}).catch(error => console.warn(error));
+        await AdMobInterstitial.showAdAsync().catch(error => console.log(error));
+    } catch (err) {
+        console.warn(err)
+    }
+}
+
+let adBanner = (<AdMobBanner
+                    bannerSize="fullBanner"
+                    adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+                    testDeviceID="EMULATOR"
+                    servePersonalizedAds // true or false
+                    onDidFailToReceiveAdWithError={this.bannerError}
+                />)
+
+
 const ResultsList = ({results, navigation}) => {
     var {state, getBrewery} = useContext(BreweryContext);
     if (results === null || results === undefined || results.length === 0) {
         return null;
     }
+    var dataList = []
+    const addAd = 5
+    for (var i = 0; i < results.length; i++ ) {
+        if (i % addAd === 0)
+            dataList.push({item: adBanner, isAd: true})
+        dataList.push({item: results[i], isAd: false})
+    }
+    console.log(dataList)
+    
     return (
         <View style={styles.container}>
             <FlatList 
-                data={results}
-                keyExtractor={(result) => result.breweryId}
+                data={dataList}
+                keyExtractor={(result, index) => result.item.breweryId}
                 renderItem={({item}) => {
+                    if (item.isAd) {
+                        return (
+                            <View style={{flex: 1}}>
+                                {item.item}
+                            </View>
+                        )
+                    }
                     return (
+                        <View style={{flex: 1}}>
                         <TouchableOpacity onPress={
                             async () => {
-                              await getBrewery({breweryId: item.breweryId})
+                              await getBrewery({breweryId: item.item.breweryId})
                               navigation.navigate("BreweryDetails")
                             }
                         }>
-                            <ResultDetails result={item}/>
+                            <ResultDetails result={item.item}/>
                         </TouchableOpacity>
+                        </View>
                     )
                 }}
                 showsHorizontalScrollIndicator={false}
